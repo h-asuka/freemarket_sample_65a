43dc79138c2230a067000a530423c61d956781af
h-asuka <h_langley_a@yahoo.co.jp>
2020-01-17 15:50:02 +0900

    エラー文を出さないように

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -7,8 +7,8 @@ $(function(){
     $('.value_1').prepend('¥')
     $('.value_2').html(profit)
     $('.value_2').prepend('¥')
-    $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
-    if(profit == '') {   // もし､計算結果が''なら表示も消す｡
+    $('#price').val(profit)
+    if(profit == '') {
     $('.value_2').html('');
     $('.value_1').html('');
     }
