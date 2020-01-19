29063cb94dcb4344730f77fde6da2d3b8f491f1d
kousaka0727 <kousaka.j@icloud.com>
2020-01-19 21:04:50 +0900

    like修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,8 @@
 class LikesController < ApplicationController
-  include SetItem
   include UserSignedIn
   before_action :user_signed_in
-  before_action :set_item
-  before_action :not_user_like
-
   def create
+    @item = Item.find(params[:item_id])
     @item.increment!(:likes_count, 1)
     @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
     @likes = Like.where(item_id: params[:item_id])
@@ -13,17 +10,10 @@ class LikesController < ApplicationController
 
 
   def destroy
+    @item = Item.find(params[:item_id])
     @item.decrement!(:likes_count, 1)
     @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
     @like.destroy
     @likes = Like.where(item_id: params[:item_id])
-  end
-
-  private
-  def  not_user_like
-    if @item.user_id == current_user.id
-      flash[:alert] = "出品者は良いねできません"
-      redirect_to root_path
-    end
   end
 end
