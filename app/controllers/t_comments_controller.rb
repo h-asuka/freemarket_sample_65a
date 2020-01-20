01b19d4d182f2e5d09c98d717f56bc3437ad4de5
kousaka0727 <kousaka.j@icloud.com>
2020-01-20 14:48:22 +0900

    軽微な修正　　コメント機能

--- app/controllers/t_comments_controller.rb
+++ app/controllers/t_comments_controller.rb
@@ -1,16 +1,10 @@
 class TCommentsController < ApplicationController
-  include UserSignedIn
-  before_action :user_signed_in
   def create
-    if @item.user_id == current_user.id || @item.buyer_id == current_user.id
-      @comment = TComment.create(comment_params)
-      respond_to do |format|
-        format.html { redirect_to transaction_item_path(@comment.item.id) }
-        format.json
-      end
-    else
-      flash[:alert] = "該当ユーザーではありません"
-      redirect_to root_path
+
+    @comment = TComment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to transaction_item_path(@comment.item.id) }
+      format.json
     end
   end
 
